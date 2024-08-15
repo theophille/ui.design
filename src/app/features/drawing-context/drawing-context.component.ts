878b79dee226a992aeba42feeddf20e142543eb9
theophille <theophille.developing@gmail.com>
2024-08-15 23:55:42 +0300

    Zoom, Pan and Fit the page

--- src/app/features/drawing-context/drawing-context.component.ts
+++ src/app/features/drawing-context/drawing-context.component.ts
@@ -1,34 +1,95 @@
-import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, inject, OnInit, ViewChild } from '@angular/core';
 import { SizesService } from '../../shared/services/sizes.service';
+import { Vec2 } from '../../shared/utils/math.utils';
+import { ZoomAndPanDirective } from './zoom-and-pan.directive';
+import { ZoomAndPanService } from './zoom-and-pan.service';
+import { PagesBoxService } from '../pages-box/pages-box.service';
+import { UIPage } from '../pages-box/page.model';
 
 @Component({
   selector: 'uid-drawing-context',
   standalone: true,
-  imports: [],
+  imports: [ZoomAndPanDirective],
   templateUrl: './drawing-context.component.html',
   styleUrl: './drawing-context.component.scss'
 })
-export class DrawingContextComponent implements AfterViewInit {
-  @ViewChild('canvas') canvas!: ElementRef<HTMLCanvasElement>;
+export class DrawingContextComponent implements OnInit, AfterViewInit {
+  @ViewChild('canvas') private canvas!: ElementRef<HTMLCanvasElement>;
+  
   private context!: CanvasRenderingContext2D | null;
+  private pageData!: UIPage;
 
-  constructor(private sizesService: SizesService) {}
+  private zoomAndPanService = inject(ZoomAndPanService);
+  private sizesService = inject(SizesService);
+  private pagesService = inject(PagesBoxService);
 
-  @HostListener('window:resize', ['$event'])
-  onResize(event:Event): void {
-    this._setCanvasSize();
+  ngOnInit(): void {
+    this.pageData = this.pagesService.selectedPageData;
+
+    this.zoomAndPanService.viewChanged.subscribe(() => {
+      this.draw();
+    });
+
+    this.pagesService.pageChanged.subscribe(() => {
+      this.pageData = this.pagesService.selectedPageData;
+
+      const scale = this.getInitialViewScale()
+      
+      this.zoomAndPanService.setOffset({
+        x: this.canvas.nativeElement.width / 2 - scale * this.pageData.width / 2,
+        y: this.canvas.nativeElement.height / 2 - scale * this.pageData.height / 2
+      });
+
+      this.zoomAndPanService.setScale(scale);
+
+      this.draw();
+    });
   }
   
   ngAfterViewInit(): void {
-    this._setCanvasSize();
+    this.setCanvasSize();
     this.context = this.canvas.nativeElement.getContext('2d');
-    console.log(this.context);
   }
 
-  private _setCanvasSize(): void {
+  private draw(): void {
+    const t = this.zoomAndPanService.getViewTransform();
+    this.context?.save();
+    this.clear();
+    this.context?.translate(t.offset.x, t.offset.y);
+    this.context?.scale(t.scale, t.scale);
+    this.context!.fillStyle = 'white';
+    this.context!.fillRect(0, 0, this.pageData.width, this.pageData.height);
+    this.context?.restore();
+  }
+  
+  private clear(): void {
+    this.context?.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+  }
+  
+  private setCanvasSize(): void {
     const navHeight: number = this.sizesService.navbarHeight;
     const contentHeight: number = window.innerHeight - navHeight - 5;
     this.canvas.nativeElement.height = contentHeight;
     this.canvas.nativeElement.width = window.innerWidth - 2 * this.sizesService.boxGroupWidth - 10;
+  }
+
+  private getInitialViewScale() {
+    let scale;
+    const canvas = this.canvas.nativeElement;
+    const pageRatio = this.pageData.width / this.pageData.height;
+    const canvasRatio = canvas.width / canvas.height;
+
+    if (pageRatio >= canvasRatio) {
+      scale = (canvas.width - 30) / this.pageData.width;
+    } else {
+      scale = (canvas.height - 30) / this.pageData.height;
+    }
+
+    return scale;
+  }
+
+  @HostListener('window:resize')
+  onResize(): void {
+    this.setCanvasSize();
   }
 }
--- src/app/features/pages-box/pages-box.service.ts
+++ src/app/features/pages-box/pages-box.service.ts
@@ -1,20 +1,39 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal, WritableSignal } from '@angular/core';
 import { UIPage } from './page.model';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PagesBoxService {
-  pages: Array<UIPage> = [];
+  private _pages: WritableSignal<Array<UIPage>> = signal([]);
+  private _selectedPage: WritableSignal<number> = signal(0);
   
   private http = inject(HttpClient);
+  
+  public pageChanged = new Subject<UIPage>();
 
   constructor() {
     console.log('hei');
     const headers = new HttpHeaders({'Content-Type': 'application/json'});
     this.http.get('http://localhost:5000/dev-project', {headers: headers}).subscribe(
-      (value) => { console.log(value) }
+      (value: any) => {
+        this._pages.set(value.pages);
+        this.pageChanged.next(this.selectedPageData);
+      }
     );
+  }
+
+  get pages(): WritableSignal<Array<UIPage>> {
+    return this._pages;
+  }
+
+  get selectedPage(): WritableSignal<number> {
+    return this._selectedPage;
+  }
+
+  get selectedPageData(): UIPage {
+    return this.pages()[this.selectedPage()];
   }
 }
